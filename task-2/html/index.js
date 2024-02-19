42cffc42e724af11ec785ca992bd32a9eab12445
tohazavr78 <antongulevec@gmail.com>
2024-02-19 18:02:38 +0300

    functions + text + colors

--- task-2/html/index.js
+++ task-2/html/index.js
@@ -1,121 +1,102 @@
 const questions = [
-  {question : 'Выберите собаку',
-    answer :{
-      a : {
-        label : 'Вы ответили верно!',
-        image : 'images/blackpuppy.jpg'
+  {
+    question: "Выберите собаку",
+    answer: {
+      a: {
+        label: "Вы ответили верно!",
+        image: "images/blackpuppy.jpg",
       },
-      b : {
-        label : 'Попробуйте ещё раз',
-        image : 'images/whitepuppy.jpg'
+      b: {
+        label: "Попробуйте ещё раз",
+        image: "images/whitepuppy.jpg",
       },
     },
-    correct :'A'
+    correct: "A",
   },
-  {question : 'Чем обработать собаку от клещей?', 
-    answer :{
-      a : {
-        label : 'Вы ответили верно!',
-        image : 'images/bravecto.jpg'
+  {
+    question: "Чем обработать собаку от клещей?",
+    answer: {
+      a: {
+        label: "Вы ответили верно!",
+        image: "images/bravecto.jpg",
       },
-      b : {
-        label : 'Попробуйте ещё раз',
-        image : 'images/bars.jpg'
+      b: {
+        label: "Попробуйте ещё раз",
+        image: "images/bars.jpg",
       },
     },
-    correct :'A'
+    correct: "A",
   },
-  {question : 'Каким кормом кормить собаку?',
-    answer :{
-      a : {
-        label : 'Попробуйте ещё раз',
-        image : 'images/pedigree.jpg'
+  {
+    question: "Каким кормом кормить собаку?",
+    answer: {
+      a: {
+        label: "Попробуйте ещё раз",
+        image: "images/pedigree.jpg",
       },
-      b : {
-        label : 'Вы ответили верно!',
-        image : 'images/grandorf.jpg'
+      b: {
+        label: "Вы ответили верно!",
+        image: "images/grandorf.jpg",
       },
     },
-  correct :'B'
-  }
-] 
+    correct: "B",
+  },
+];
 
 let questionIndex = 0;
-let correctAnswer = questions[questionIndex]['correct'];
-
+let correctAnswer = questions[questionIndex]["correct"];
+let answerA = document.getElementById("answer-A");
+let answerB = document.getElementById("answer-B");
+let textA = document.getElementById("text-A");
+let textB = document.getElementById("text-B");
 
 showQuestion(questionIndex);
 
+function showQuestion() {
+  document.getElementById("question").innerHTML =
+    questions[questionIndex]["question"];
+  document
+    .getElementById("image-A")
+    .setAttribute("src", questions[questionIndex]["answer"].a.image);
+  document
+    .getElementById("image-B")
+    .setAttribute("src", questions[questionIndex]["answer"].b.image);
+  correctAnswer = questions[questionIndex]["correct"];
+}
 
-
-function showQuestion(){
-document.getElementById('question').innerHTML = questions[questionIndex]['question'];
-document.getElementById('image-A').setAttribute('src',questions[questionIndex]['answer'].a.image) ;
-document.getElementById('image-B').setAttribute('src',questions[questionIndex]['answer'].b.image) ;
-correctAnswer = questions[questionIndex]['correct'];
-};
-
-let container = document.getElementById('container');
-container.onclick = function(event){
-  let target = event.target;
-  let answer = target.dataset.answer;
-  if (target.className != "answer") return;
-  if(answer == correctAnswer){
-      target.style.background = 'green';
-      setTimeout(() => target.style.background = '',3000);
-      alert('Вы ответили верно!');
-      if (questionIndex !== questions.length - 1){
-      setTimeout(() => showQuestion(questionIndex++),3000);
-      }else{
-        setTimeout(()=> window.location.href ='finalPage.html',3000);
-      }
-  }else{
-      target.style.background = 'red';
-      setTimeout(() => target.style.background = '',3000);
-      alert('Вы ошиблись, попробуйте ещё раз');
+function onRightAnswerClick() {
+  if (correctAnswer == "A") {
+    textA.innerHTML = questions[questionIndex]["answer"].a.label;
+    answerA.style.background = "#E3FFEE";
+    setTimeout(() => {
+      textA.innerHTML = "";
+      answerA.style.background = "";
+    }, 3000);
   }
-  };
-
-/*function onRightAnswerClick(){
-  let target = Event.target;
-  if ( target.dataset.answer == A){
-    document.getElementById('text-A').innerText = questions[questionIndex]['answer'].a.label;
-    setTimeout(()=> document.getElementById('text-A').innerText ='',3000);
-    document.getElementById('answer-A').style.backgroundColor = 'green';
-    setTimeout(()=>document.getElementById('answer-A').style.backgroundColor = '',3000);
+  if (correctAnswer == "B") {
+    textB.innerHTML = questions[questionIndex]["answer"].b.label;
+    answerB.style.background = "#E3FFEE";
+    setTimeout(() => {
+      textB.innerHTML = "";
+      answerB.style.background = "";
+    }, 3000);
   }
-  if ( answer == B){
-    document.getElementById('text-B').innerText = questions[questionIndex]['answer'].B.label;
-    setTimeout(()=> document.getElementById('text-A').innerText ='',3000);
-    document.getElementById('answer-B').style.backgroundColor = 'green';
-    setTimeout(()=>document.getElementById('answer-A').style.backgroundColor = '',3000);
-};
-setTimeout(()=> showQuestion(questionIndex++),3000);
-console.log(target);
-};*/
+  questionIndex !== questions.length - 1
+    ? setTimeout(() => showQuestion(questionIndex++), 3000)
+    : setTimeout(() => (window.location.href = "finalPage.html"), 3000);
+}
 
+function onWrongAnswerClick() {
+  correctAnswer !== "A"
+    ? (answerA.style.background = "#FFEAE3")
+    : (answerB.style.background = "#FFEAE3");
+  setTimeout(() => {
+    answerA.style.background = "";
+    answerB.style.background = "";
+  }, 3000);
+  alert("Вы ошиблись, попробуйте ещё раз");
+}
 
-/*function onAnswerClick (A,B) {
-  if (A == correctAnswer || B == correctAnswer){
-    onRightAnswerClick();
-    setTimeout(()=> showQuestion(questionIndex++),3000);
-  } 
-  else {
-    onWrongAnswerClick();
-  };
-};*/
-
-/*function onRightAnswerClick(A,B){
-document.getElementById('text-A').innerText = questions[questionIndex]['answer'].a.label;
-setTimeout(()=> document.getElementById('text-A').innerText ='',3000);
-document.getElementById('answer-A').style.backgroundColor = 'green';
-setTimeout(()=>document.getElementById('answer-A').style.backgroundColor = '',3000);
-setTimeout(() => showQuestion(questionIndex++),3000);
-};
-
-function onWrongAnswerClick(){
-document.getElementById('answer-B').style.backgroundColor = 'red';
-setTimeout(()=>document.getElementById('answer-B').style.backgroundColor = '',3000);
-alert('Вы ошиблись, попробуйте ещё раз');
-};*/
-
+function onAnswerClick(answer) {
+  answer == correctAnswer ? onRightAnswerClick() : onWrongAnswerClick();
+}
